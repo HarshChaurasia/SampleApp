1bcbee26d0a6bd885ed573118a753e408c217deb
HarshChaurasia <harshchaurasia789@gmail.com>
2023-05-25 11:36:39 +0530

    uploading project

---
+++ SampleApp/SampleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
